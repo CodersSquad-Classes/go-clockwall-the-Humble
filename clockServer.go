2b29d9fb4653e5fada8095f7ba63134120ae2182
Jose Ignacio Ferrer <joseferrertec@gmail.com>
2022-05-11 03:51:16 +0000

    Clock wall working

--- clockServer.go
+++ clockServer.go
@@ -2,28 +2,59 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
+func TimeIn(t time.Time, name string) (time.Time, error) {
+	loc, err := time.LoadLocation(name)
+	if err == nil {
+		t = t.In(loc)
+	}
+	return t, err
+}
+
 func handleConn(c net.Conn) {
 	defer c.Close()
+
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return // e.g., client disconnected
+		var timeObj, timeErr = TimeIn(time.Now(), os.Getenv("TZ"))
+		if timeErr == nil {
+			_, err := io.WriteString(c, os.Getenv("TZ")+"\t= "+timeObj.Local().Format("15:04:05\n"))
+			if err != nil {
+				return // e.g., client disconnected
+			}
+			time.Sleep(1 * time.Second)
+		} else {
+			fmt.Println("Invalid timezone")
+			fmt.Println()
+			os.Exit(-1)
 		}
-		time.Sleep(1 * time.Second)
 	}
+
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run clockServer.go -port <portNum>")
+		fmt.Println()
+		os.Exit(-1)
+	}
+
+	var portNum = flag.String("port", "9090", "Port specified for clock connection")
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", "localhost:"+*portNum)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
--- clockWall.go
+++ clockWall.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func displayTime(port string, done chan string) {
+
+	conn, err := net.Dial("tcp", "localhost:"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mustCopy(os.Stdout, conn)
+	done <- port
+}
+
 func main() {
 	// Let's start the fun
+	done := make(chan string, 3)
+	for _, element := range os.Args[1:] {
+		var port = strings.Split(element, ":")[1]
+		go displayTime(port, done)
+	}
+	_ = <-done
 }
